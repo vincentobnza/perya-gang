f6d49f880b53a6c297ce439c60c0e76ad58a5678
Vincent#05 <vincentobenza2003@gmail.com>
2025-07-23 21:49:12 +0800

    Added validation

--- app/api/validate.ts
+++ app/api/validate.ts
@@ -1,30 +1,31 @@
 import axios from "axios";
 import { apiUrl } from "@/lib/common";
-import { storeUserData, getUserData }from "@/lib/hooks/useLocalStorage"
+import { storeUserData, getUserData } from "@/lib/hooks/useLocalStorage";
 
-export async function validateLogin({}, callback?: (success: boolean, message: string) => void
+export async function validateLogin(
+  {},
+  callback?: (success: boolean, message: string) => void
 ): Promise<void> {
   try {
-     axios.defaults.headers.common['X-Session-Token'] = getUserData().token;
+    axios.defaults.headers.common["X-Session-Token"] = getUserData().token;
     const response = await axios.post(apiUrl("auth/user"), {});
     if (response.status !== 200) {
-
       callback?.(false, "Failed to register account");
       return;
     }
     const _user = {
-        status: true,
-        f_: response.data.name,
-        e_: response.data.email,
-        u_: response.data.username,
-        id_: response.data.id,
-        balances: response.data.user_balance,
-        token: getUserData().token,
-    }
+      status: true,
+      f_: response.data.name,
+      e_: response.data.email,
+      u_: response.data.username,
+      id_: response.data.id,
+      balances: response.data.user_balance,
+      token: getUserData().token,
+    };
     console.log("User data:", _user);
     storeUserData(_user);
 
-    axios.defaults.headers.common['X-Session-Token'] = _user.token;
+    axios.defaults.headers.common["X-Session-Token"] = _user.token;
 
     callback?.(true, "Login successful");
   } catch (error: any) {
