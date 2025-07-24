c4f9a76137959ff4ddd12c63d39d31ca7aa98381
glen0xFFF <025glenox025@gmail.com>
2025-07-24 21:59:49 +0800

    fix navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,42 +2,35 @@
 import NavbarBtns from "@/components/NavbarBtns";
 import Image from "next/image";
 import Link from "next/link";
-import { validateLogin } from "./api/validate";
-import { setToast } from "@/lib/common";
-import { getUserData } from "@/lib/hooks/useLocalStorage";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getUserData } from "@/lib/hooks/useLocalStorage";
 
-export default function Navbar() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState("Guest");
-
+export default function NavbarClient() {
+  const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+  const user = getUserData();
   useEffect(() => {
-    validateLogin({}, (success, message) => {
-      console.log("Validation callback:", success, message);
-      if (success) {
-        const userData = getUserData();
-        console.log("User data:", userData);
-        setIsAuthenticated(true);
-        setUser(userData?.f_ || "Guest");
-      } else {
-        setIsAuthenticated(false);
-        setUser("Guest");
-      }
-    });
-  }, []);
+    setIsLogin(!!user);
+  }, [user]);
 
   return (
     <nav className="w-full h-24 grid place-items-center border-b border-zinc-900">
       <div className="w-full max-w-md md:max-w-screen-xl mx-auto flex justify-between items-center">
         <Link href="/">
-          <Image src="/LOGO.png" width={50} height={50} alt="Logo" />
+          <Image
+            src="/LOGO.png"
+            width={50}
+            height={50}
+            alt="Logo"
+          />
         </Link>
-        {isAuthenticated && (
-          <h1 className="text-sm md:text-[15px] lg:text-lg opacity-70">
-            Hello {user}!
-          </h1>
+
+        {isLogin && (
+          <h1 className="text-sm md:text-lg lg:text-xl">Hello {user.f_}!</h1>
         )}
-        <NavbarBtns isAuthenticated={isAuthenticated} />
+
+        <NavbarBtns isAuthenticated={isLogin} />
       </div>
     </nav>
   );
